52ed41e285ad1968b589d54c0a2c863c63284f0b
Jaime Chávez <jaime-mcr@hotmail.com>
2016-02-18 15:49:37 -0600

    Mal Sondeo :(

--- api/php/sondeo.php
+++ api/php/sondeo.php
@@ -4,8 +4,38 @@ include('funciones.php');
 
 date_default_timezone_set('America/El_Salvador');
 
-echo json_encode(array(
-				'subsecciones' => subseccion(1)
-				));
+$soninfo = mysql_query("SELECT idSondeo, titulo, pregunta, fecha, rutaFoto as foto
+											FROM sondeo
+											ORDER BY fecha desc, hora desc");
+$resinfo = mysql_query("SELECT idRespuesta, idSondeo, respuesta
+											FROM respuesta");
 
+
+
+while ($sonpreview = mysql_fetch_array($soninfo)) {
+	$sondeo[] = array(
+							'idSondeo' => $sonpreview['idSondeo'],
+							'titulo' => $sonpreview['titulo'],
+							'pregunta' => $sonpreview['pregunta'],
+							'fecha' => $sonpreview['fecha'],
+							'foto' => $sonpreview['foto'],
+							'respuestas' => respuestas($resinfo, $sonpreview['idSondeo'])
+	);
+}
+function respuestas($info, $id){
+	while ($respreview = mysql_fetch_array($info)) {
+		if($id == $respreview['idSondeo']){
+			$respuestas[] = array(
+								'idRespuesta' => $respreview['idRespuesta'],
+								'idSondeo' => $respreview['idSondeo'],
+								'respuesta' => $respreview['respuesta'],
+		);
+	}
+	}
+	return $respuestas;
+}
+echo json_encode(array(
+				'subsecciones' => subseccion(1),
+				'sondeos' => $sondeo
+				));
 ?>
